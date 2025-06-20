acsone/wms

[MIG] s_a_to_promise_release: fix schedule date change

stock_available_to_promise_release/models/stock_picking.py
@@ -156,7 +156,7 @@ class StockPicking(models.Model):
         new_expected_date = fields.Datetime.add(
             fields.Datetime.now(), minutes=prep_time
         )
-        move_to_update = self.move_lines.filtered(
+        move_to_update = self.move_line_ids.filtered(
             lambda m: m.state in ["assigned", "confirmed", "partially_available"]
         )
         move_to_update_ids = move_to_update.ids

OCA/stock-logistics-reservation

stock_available_to_promise_release/models/stock_picking.py
@@ -156,7 +156,7 @@ class StockPicking(models.Model):
         new_expected_date = fields.Datetime.add(
             fields.Datetime.now(), minutes=prep_time
         )
-        move_to_update = self.move_lines.filtered(
+        move_to_update = self.move_ids.filtered(
             lambda m: m.state in ["assigned", "confirmed", "partially_available"]
         )
         move_to_update_ids = move_to_update.ids