OCA/wms

[IMP] stock_available_to_promise_release: Multi warehouse capability
Compute previous_promised_qty and ordered_available_to_promise_qty considering warehouse

stock_available_to_promise_release/models/stock_move.py
@@ -9,7 +9,7 @@ import operator as py_operator
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.osv import expression
-from odoo.tools import date_utils, float_compare, float_round
+from odoo.tools import date_utils, float_compare, float_round, groupby
 
 _logger = logging.getLogger(__name__)
 
@@ -180,8 +180,8 @@ class StockMove(models.Model):
     def _previous_promised_qty_sql_moves_no_release(self):
         return "m.need_release IS false OR m.need_release IS null"
 
-    def _previous_promised_qty_sql_lateral_where(self):
-        locations = self._ordered_available_to_promise_locations()
+    def _previous_promised_qty_sql_lateral_where(self, warehouse):
+        locations = warehouse.view_location_id
         sql = """
                 m.id != move.id
                 AND m.product_id = move.product_id
@@ -215,33 +215,50 @@ class StockMove(models.Model):
             params["horizon"] = horizon_date
         return sql, params
 
-    def _previous_promised_qty_sql(self):
+    def _previous_promised_qty_sql(self, warehouse):
         """Lookup query for product promised qty in the same warehouse.
 
         Moves to consider are either already released or still be to released
         but not done yet. Each of them should fit the reservation horizon.
         """
         params = {"move_ids": tuple(self.ids)}
-        lateral_where, lateral_params = self._previous_promised_qty_sql_lateral_where()
+        lateral_where, lateral_params = self._previous_promised_qty_sql_lateral_where(
+            warehouse
+        )
         params.update(lateral_params)
         query = self._previous_promised_qty_sql_main_query().format(
             lateral_where=lateral_where
         )
         return query, params
 
+    def _group_by_warehouse(self):
+        return groupby(self, lambda m: m.warehouse_id)
+
+    def _get_previous_promised_qties(self):
+        self.flush()
+        self.env["stock.move.line"].flush(["move_id", "product_qty"])
+        self.env["stock.location"].flush(["parent_path"])
+        previous_promised_qties = {}
+        for warehouse, moves in self._group_by_warehouse():
+            moves = self.browse().union(*moves)
+            if not warehouse:
+                for move in moves:
+                    previous_promised_qties[move.id] = 0
+                continue
+            query, params = moves._previous_promised_qty_sql(warehouse)
+            self.env.cr.execute(query, params)
+            rows = dict(self.env.cr.fetchall())
+            previous_promised_qties.update(rows)
+        return previous_promised_qties
+
     @api.depends()
     def _compute_previous_promised_qty(self):
         if not self.ids:
             return
-        self.flush()
-        self.env["stock.move.line"].flush(["move_id", "product_qty"])
-        self.env["stock.location"].flush(["parent_path"])
-        self.previous_promised_qty = 0
-        query, params = self._previous_promised_qty_sql()
-        self.env.cr.execute(query, params)
-        rows = dict(self.env.cr.fetchall())
+        previous_promised_qty_by_move = self._get_previous_promised_qties()
         for move in self:
-            move.previous_promised_qty = rows.get(move.id, 0)
+            previous_promised_qty = previous_promised_qty_by_move.get(move.id, 0)
+            move.previous_promised_qty = previous_promised_qty
 
     def _is_release_needed(self):
         self.ensure_one()
@@ -294,9 +311,6 @@ class StockMove(models.Model):
         moves = moves.filtered(lambda m: m.release_ready)
         return [("id", "in", moves.ids)]
 
-    def _ordered_available_to_promise_locations(self):
-        return self.env["stock.warehouse"].search([]).mapped("view_location_id")
-
     @api.depends()
     def _compute_ordered_available_to_promise(self):
         moves = self.filtered(
@@ -308,55 +322,63 @@ class StockMove(models.Model):
                 "ordered_available_to_promise_uom_qty": 0.0,
             }
         )
-
-        locations = moves._ordered_available_to_promise_locations()
-
+        moves_by_warehouse = moves._group_by_warehouse()
         # Compute On-Hand quantity (equivalent of qty_available) for all "view
         # locations" of all the warehouses: we may release as soon as we have
         # the quantity somewhere. Do not use "qty_available" to get a faster
         # computation.
-        location_domain = []
-        for location in locations:
-            location_domain = expression.OR(
-                [
-                    location_domain,
-                    [("location_id.parent_path", "=like", location.parent_path + "%")],
-                ]
-            )
-        domain_quant = expression.AND(
-            [[("product_id", "in", moves.product_id.ids)], location_domain]
-        )
-        location_quants = self.env["stock.quant"].read_group(
-            domain_quant, ["product_id", "quantity"], ["product_id"], orderby="id"
-        )
-        quants_available = {
-            item["product_id"][0]: item["quantity"] for item in location_quants
-        }
-        for move in moves:
-            product_uom = move.product_id.uom_id
-            previous_promised_qty = move.previous_promised_qty
+        for warehouse, _moves in moves_by_warehouse:
+            _moves = self.browse().union(*_moves)
+            if not warehouse:
+                moves.write(
+                    {
+                        "ordered_available_to_promise_uom_qty": 0,
+                        "ordered_available_to_promise_qty": 0,
+                    }
+                )
+                continue
 
-            rounding = product_uom.rounding
-            available_qty = float_round(
-                quants_available.get(move.product_id.id, 0.0),
-                precision_rounding=rounding,
+            location_domain = [
+                (
+                    "location_id.parent_path",
+                    "=like",
+                    warehouse.view_location_id.parent_path + "%",
+                )
+            ]
+            domain_quant = expression.AND(
+                [[("product_id", "in", moves.product_id.ids)], location_domain]
             )
+            location_quants = self.env["stock.quant"].read_group(
+                domain_quant, ["product_id", "quantity"], ["product_id"], orderby="id"
+            )
+            quants_available = {
+                item["product_id"][0]: item["quantity"] for item in location_quants
+            }
+            for move in _moves:
+                product_uom = move.product_id.uom_id
+                previous_promised_qty = move.previous_promised_qty
 
-            real_promised = available_qty - previous_promised_qty
-            uom_promised = product_uom._compute_quantity(
-                real_promised,
-                move.product_uom,
-                rounding_method="HALF-UP",
-            )
+                rounding = product_uom.rounding
+                available_qty = float_round(
+                    quants_available.get(move.product_id.id, 0.0),
+                    precision_rounding=rounding,
+                )
 
-            move.ordered_available_to_promise_uom_qty = max(
-                min(uom_promised, move.product_uom_qty),
-                0.0,
-            )
-            move.ordered_available_to_promise_qty = max(
-                min(real_promised, move.product_qty),
-                0.0,
-            )
+                real_promised = available_qty - previous_promised_qty
+                uom_promised = product_uom._compute_quantity(
+                    real_promised,
+                    move.product_uom,
+                    rounding_method="HALF-UP",
+                )
+
+                move.ordered_available_to_promise_uom_qty = max(
+                    min(uom_promised, move.product_uom_qty),
+                    0.0,
+                )
+                move.ordered_available_to_promise_qty = max(
+                    min(real_promised, move.product_qty),
+                    0.0,
+                )
 
     def _search_ordered_available_to_promise_uom_qty(self, operator, value):
         operator_mapping = {

stock_available_to_promise_release/tests/test_reservation.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Camptocamp (https://www.camptocamp.com)
+# Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from datetime import datetime
@@ -1145,49 +1146,10 @@ class TestAvailableToPromiseRelease(PromiseReleaseCommonCase):
     # TODO: test w/ multiple orders by priority
 
     def test_picking_priority(self):
-        self.pick_type = self.env.ref("stock.picking_type_out")
-        self.loc_output = self.env.ref("stock.stock_location_output")
-        self.route = self.env["stock.location.route"].create(
-            {
-                "name": "test route",
-                "company_id": self.env.company.id,
-                "sequence": 10,
-            }
-        )
-        self.product1.route_ids = [(6, 0, [self.route.id])]
-        self.rule = self.env["stock.rule"].create(
-            {
-                "name": "pull rule test",
-                "action": "pull",
-                "location_src_id": self.loc_customer.id,
-                "location_id": self.loc_output.id,
-                "route_id": self.route.id,
-                "picking_type_id": self.pick_type.id,
-                "procure_method": "make_to_order",
-            }
-        )
+        self.wh.delivery_steps = "pick_pack_ship"
+        self.wh.delivery_route_id.write({"available_to_promise_defer_pull": True})
         self._update_qty_in_location(self.loc_bin1, self.product1, 20.0)
-        self._update_qty_in_location(self.loc_stock, self.product1, 20.0)
-        pick = self.env["stock.picking"].create(
-            {
-                "picking_type_id": self.pick_type.id,
-                "location_id": self.loc_output.id,
-                "location_dest_id": self.loc_customer.id,
-                "move_lines": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": self.product1.name,
-                            "product_id": self.product1.id,
-                            "product_uom_qty": 5.0,
-                            "product_uom": self.product1.uom_id.id,
-                            "need_release": True,
-                        },
-                    )
-                ],
-            }
-        )
+        pick = self._create_picking_chain(self.wh, [(self.product1, 20)])
         pick.priority = "1"
         pick.action_confirm()
         pick.release_available_to_promise()
@@ -1237,3 +1199,32 @@ class TestAvailableToPromiseRelease(PromiseReleaseCommonCase):
         picking.release_available_to_promise()
         new_picking = self._pickings_in_group(picking.group_id) - picking
         self.assertEqual(new_picking.move_type, "one")
+
+    def test_multiple_warehouses(self):
+        wh2 = self.wh.create({"name": "Warehouse2", "code": "wh2"})
+        warehouses = self.wh | wh2
+        warehouses.delivery_route_id.write({"available_to_promise_defer_pull": True})
+
+        self._update_qty_in_location(self.wh.lot_stock_id, self.product1, 2)
+        self._update_qty_in_location(wh2.lot_stock_id, self.product1, 2)
+
+        picking_wh1_1 = self._create_picking_chain(self.wh, [(self.product1, 4)])
+        picking_wh2_1 = self._create_picking_chain(wh2, [(self.product1, 4)])
+
+        self.assertEqual(picking_wh1_1.move_lines.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh1_1.move_lines.previous_promised_qty, 0)
+        self.assertEqual(picking_wh2_1.move_lines.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh2_1.move_lines.previous_promised_qty, 0)
+
+        picking_wh1_2 = self._create_picking_chain(self.wh, [(self.product1, 4)])
+        picking_wh2_2 = self._create_picking_chain(wh2, [(self.product1, 4)])
+
+        self.assertEqual(picking_wh1_1.move_lines.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh1_1.move_lines.previous_promised_qty, 0)
+        self.assertEqual(picking_wh2_1.move_lines.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh2_1.move_lines.previous_promised_qty, 0)
+
+        self.assertEqual(picking_wh1_2.move_lines.ordered_available_to_promise_qty, 0)
+        self.assertEqual(picking_wh1_2.move_lines.previous_promised_qty, 4)
+        self.assertEqual(picking_wh2_2.move_lines.ordered_available_to_promise_qty, 0)
+        self.assertEqual(picking_wh2_2.move_lines.previous_promised_qty, 4)

OCA/stock-logistics-reservation

stock_available_to_promise_release/models/stock_move.py
@@ -9,7 +9,7 @@ import operator as py_operator
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.osv import expression
-from odoo.tools import date_utils, float_compare, float_round
+from odoo.tools import date_utils, float_compare, float_round, groupby
 
 _logger = logging.getLogger(__name__)
 
@@ -189,8 +189,8 @@ class StockMove(models.Model):
     def _previous_promised_qty_sql_moves_no_release(self):
         return "m.need_release IS false OR m.need_release IS null"
 
-    def _previous_promised_qty_sql_lateral_where(self):
-        locations = self._ordered_available_to_promise_locations()
+    def _previous_promised_qty_sql_lateral_where(self, warehouse):
+        locations = warehouse.view_location_id
         sql = """
                 m.id != move.id
                 AND m.product_id = move.product_id
@@ -224,19 +224,41 @@ class StockMove(models.Model):
             params["horizon"] = horizon_date
         return sql, params
 
-    def _previous_promised_qty_sql(self):
+    def _previous_promised_qty_sql(self, warehouse):
         """Lookup query for product promised qty in the same warehouse.
 
         Moves to consider are either already released or still be to released
         but not done yet. Each of them should fit the reservation horizon.
         """
         params = {"move_ids": tuple(self.ids)}
-        lateral_where, lateral_params = self._previous_promised_qty_sql_lateral_where()
+        lateral_where, lateral_params = self._previous_promised_qty_sql_lateral_where(
+            warehouse
+        )
         params.update(lateral_params)
         query = self._previous_promised_qty_sql_main_query().format(
             lateral_where=lateral_where
         )
         return query, params
+
+    def _group_by_warehouse(self):
+        return groupby(self, lambda m: m.warehouse_id)
+
+    def _get_previous_promised_qties(self):
+        self.env.flush_all()
+        self.env["stock.move.line"].flush_model(["move_id", "reserved_qty"])
+        self.env["stock.location"].flush_model(["parent_path"])
+        previous_promised_qties = {}
+        for warehouse, moves in self._group_by_warehouse():
+            moves = self.browse().union(*moves)
+            if not warehouse:
+                for move in moves:
+                    previous_promised_qties[move.id] = 0
+                continue
+            query, params = moves._previous_promised_qty_sql(warehouse)
+            self.env.cr.execute(query, params)
+            rows = dict(self.env.cr.fetchall())
+            previous_promised_qties.update(rows)
+        return previous_promised_qties
 
     # As we don't set depends here, we need to invalidate cache before
     # accessing the computed value.
@@ -245,15 +267,10 @@ class StockMove(models.Model):
     def _compute_previous_promised_qty(self):
         if not self.ids:
             return
-        self.env.flush_all()
-        self.env["stock.move.line"].flush_model(["move_id", "reserved_qty"])
-        self.env["stock.location"].flush_model(["parent_path"])
-        self.previous_promised_qty = 0
-        query, params = self._previous_promised_qty_sql()
-        self.env.cr.execute(query, params)
-        rows = dict(self.env.cr.fetchall())
+        previous_promised_qty_by_move = self._get_previous_promised_qties()
         for move in self:
-            move.previous_promised_qty = rows.get(move.id, 0)
+            previous_promised_qty = previous_promised_qty_by_move.get(move.id, 0)
+            move.previous_promised_qty = previous_promised_qty
 
     def _is_release_needed(self):
         self.ensure_one()
@@ -308,9 +325,6 @@ class StockMove(models.Model):
         moves = moves.filtered(lambda m: m.release_ready)
         return [("id", "in", moves.ids)]
 
-    def _ordered_available_to_promise_locations(self):
-        return self.env["stock.warehouse"].search([]).mapped("view_location_id")
-
     @api.depends()
     def _compute_ordered_available_to_promise(self):
         moves = self.filtered(
@@ -322,48 +336,57 @@ class StockMove(models.Model):
                 "ordered_available_to_promise_uom_qty": 0.0,
             }
         )
-
-        locations = moves._ordered_available_to_promise_locations()
-
+        moves_by_warehouse = moves._group_by_warehouse()
         # Compute On-Hand quantity (equivalent of qty_available) for all "view
         # locations" of all the warehouses: we may release as soon as we have
         # the quantity somewhere. Do not use "qty_available" to get a faster
         # computation.
-        location_domain = locations._get_available_to_promise_domain()
-        domain_quant = expression.AND(
-            [[("product_id", "in", moves.product_id.ids)], location_domain]
-        )
-        location_quants = self.env["stock.quant"].read_group(
-            domain_quant, ["product_id", "quantity"], ["product_id"], orderby="id"
-        )
-        quants_available = {
-            item["product_id"][0]: item["quantity"] for item in location_quants
-        }
-        for move in moves:
-            product_uom = move.product_id.uom_id
-            previous_promised_qty = move.previous_promised_qty
+        for warehouse, _moves in moves_by_warehouse:
+            _moves = self.browse().union(*_moves)
+            if not warehouse:
+                moves.write(
+                    {
+                        "ordered_available_to_promise_uom_qty": 0,
+                        "ordered_available_to_promise_qty": 0,
+                    }
+                )
+                continue
 
-            rounding = product_uom.rounding
-            available_qty = float_round(
-                quants_available.get(move.product_id.id, 0.0),
-                precision_rounding=rounding,
+            location_domain = warehouse.view_location_id._get_available_to_promise_domain()
+            domain_quant = expression.AND(
+                [[("product_id", "in", moves.product_id.ids)], location_domain]
             )
+            location_quants = self.env["stock.quant"].read_group(
+                domain_quant, ["product_id", "quantity"], ["product_id"], orderby="id"
+            )
+            quants_available = {
+                item["product_id"][0]: item["quantity"] for item in location_quants
+            }
+            for move in _moves:
+                product_uom = move.product_id.uom_id
+                previous_promised_qty = move.previous_promised_qty
 
-            real_promised = available_qty - previous_promised_qty
-            uom_promised = product_uom._compute_quantity(
-                real_promised,
-                move.product_uom,
-                rounding_method="HALF-UP",
-            )
+                rounding = product_uom.rounding
+                available_qty = float_round(
+                    quants_available.get(move.product_id.id, 0.0),
+                    precision_rounding=rounding,
+                )
 
-            move.ordered_available_to_promise_uom_qty = max(
-                min(uom_promised, move.product_uom_qty),
-                0.0,
-            )
-            move.ordered_available_to_promise_qty = max(
-                min(real_promised, move.product_qty),
-                0.0,
-            )
+                real_promised = available_qty - previous_promised_qty
+                uom_promised = product_uom._compute_quantity(
+                    real_promised,
+                    move.product_uom,
+                    rounding_method="HALF-UP",
+                )
+
+                move.ordered_available_to_promise_uom_qty = max(
+                    min(uom_promised, move.product_uom_qty),
+                    0.0,
+                )
+                move.ordered_available_to_promise_qty = max(
+                    min(real_promised, move.product_qty),
+                    0.0,
+                )
 
     def _search_ordered_available_to_promise_uom_qty(self, operator, value):
         operator_mapping = {

stock_available_to_promise_release/tests/test_reservation.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Camptocamp (https://www.camptocamp.com)
+# Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from datetime import datetime
@@ -1130,51 +1131,10 @@ class TestAvailableToPromiseRelease(PromiseReleaseCommonCase):
     # TODO: test w/ multiple orders by priority
 
     def test_picking_priority(self):
-        self.pick_type = self.env.ref("stock.picking_type_out")
-        self.loc_output = self.env.ref("stock.stock_location_output")
-        self.route = self.env["stock.route"].create(
-            {
-                "name": "test route",
-                "company_id": self.env.company.id,
-                "sequence": 10,
-            }
-        )
-        self.product1.route_ids = [(6, 0, [self.route.id])]
-        self.rule = self.env["stock.rule"].create(
-            {
-                "name": "pull rule test",
-                "action": "pull",
-                "location_src_id": self.loc_customer.id,
-                "location_dest_id": self.loc_output.id,
-                "route_id": self.route.id,
-                "picking_type_id": self.pick_type.id,
-                "procure_method": "make_to_order",
-            }
-        )
+        self.wh.delivery_steps = "pick_pack_ship"
+        self.wh.delivery_route_id.write({"available_to_promise_defer_pull": True})
         self._update_qty_in_location(self.loc_bin1, self.product1, 20.0)
-        self._update_qty_in_location(self.loc_stock, self.product1, 20.0)
-        pick = self.env["stock.picking"].create(
-            {
-                "picking_type_id": self.pick_type.id,
-                "location_id": self.loc_output.id,
-                "location_dest_id": self.loc_customer.id,
-                "move_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": self.product1.name,
-                            "product_id": self.product1.id,
-                            "product_uom_qty": 5.0,
-                            "product_uom": self.product1.uom_id.id,
-                            "need_release": True,
-                            "location_id": self.loc_output.id,
-                            "location_dest_id": self.loc_customer.id,
-                        },
-                    )
-                ],
-            }
-        )
+        pick = self._create_picking_chain(self.wh, [(self.product1, 20)])
         pick.priority = "1"
         pick.action_confirm()
         pick.release_available_to_promise()
@@ -1203,3 +1163,32 @@ class TestAvailableToPromiseRelease(PromiseReleaseCommonCase):
         self.assertTrue(picking.release_ready)
         picking.action_cancel()
         self.assertFalse(picking.release_ready)
+
+    def test_multiple_warehouses(self):
+        wh2 = self.wh.create({"name": "Warehouse2", "code": "wh2"})
+        warehouses = self.wh | wh2
+        warehouses.delivery_route_id.write({"available_to_promise_defer_pull": True})
+
+        self._update_qty_in_location(self.wh.lot_stock_id, self.product1, 2)
+        self._update_qty_in_location(wh2.lot_stock_id, self.product1, 2)
+
+        picking_wh1_1 = self._create_picking_chain(self.wh, [(self.product1, 4)])
+        picking_wh2_1 = self._create_picking_chain(wh2, [(self.product1, 4)])
+
+        self.assertEqual(picking_wh1_1.move_ids.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh1_1.move_ids.previous_promised_qty, 0)
+        self.assertEqual(picking_wh2_1.move_ids.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh2_1.move_ids.previous_promised_qty, 0)
+
+        picking_wh1_2 = self._create_picking_chain(self.wh, [(self.product1, 4)])
+        picking_wh2_2 = self._create_picking_chain(wh2, [(self.product1, 4)])
+
+        self.assertEqual(picking_wh1_1.move_ids.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh1_1.move_ids.previous_promised_qty, 0)
+        self.assertEqual(picking_wh2_1.move_ids.ordered_available_to_promise_qty, 2)
+        self.assertEqual(picking_wh2_1.move_ids.previous_promised_qty, 0)
+
+        self.assertEqual(picking_wh1_2.move_ids.ordered_available_to_promise_qty, 0)
+        self.assertEqual(picking_wh1_2.move_ids.previous_promised_qty, 4)
+        self.assertEqual(picking_wh2_2.move_ids.ordered_available_to_promise_qty, 0)
+        self.assertEqual(picking_wh2_2.move_ids.previous_promised_qty, 4)